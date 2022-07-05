d67970f2f00dbd57a6976e48ce86a8c6d8963dfd
Malinimozhiyan <malinimanimozhiyan@gmail.com>
2022-07-05 13:09:59 +0000

    implemented the perimeter method using double datatype

--- guru-shifu/Rectangle/src/main/java/com/thoughtworks/rectangle/Rectangle.java
+++ guru-shifu/Rectangle/src/main/java/com/thoughtworks/rectangle/Rectangle.java
@@ -1,9 +1,21 @@
 package com.thoughtworks.rectangle;
 
 public class Rectangle {
-    public int FindArea(int length,int breadth)
+      private final double length;
+      private final double breadth;
+    public Rectangle(double length,double breadth)
     {
-      int Area=length*breadth;
-      return Area;
+      this.length=length;
+      this.breadth=breadth;
+    }
+    public double area()
+    {
+      double answer=length*breadth;
+      return answer;
+    }
+    public double perimeter()
+    {
+      double perimeteranswer=2*(length+breadth);
+      return perimeteranswer;
     }
 }
--- guru-shifu/Rectangle/src/test/java/com/thoughtworks/rectangle/RectangleTest.java
+++ guru-shifu/Rectangle/src/test/java/com/thoughtworks/rectangle/RectangleTest.java
@@ -2,14 +2,21 @@ package com.thoughtworks.rectangle;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-
+import static org.hamcrest.Matchers.*;
 import org.junit.jupiter.api.Test;
 public class RectangleTest {
     @Test 
-    void Should_Return_Area_When_Length_And_Breadth_Are_Given()
+    void shouldReturnAreaWhenLengthAndBreadthAreGiven()
     {
-        Rectangle rectangle= new Rectangle();
-        int Result=rectangle.FindArea(2, 3);
-        assertThat(Result,equalTo(6));
+        Rectangle rectangle= new Rectangle(2.0,3.0);
+        double result=rectangle.area();
+        assertThat(result,closeTo(6.0,0.1));
      }
+     @Test 
+     void shouldReturnPerimeterWhenLengthAndBreadthAreGiven()
+     {
+         Rectangle rectangle= new Rectangle(2.0,3.0);
+         double perimeteranswer=rectangle.perimeter();
+         assertThat(perimeteranswer,closeTo(10.0,0.1));
+      }
 }
